7888ac105d950747f12080513091a00d7da449aa
devsojitra37 <devanshsojitra37@gmail.com>
2024-06-30 22:38:58 +0530

    Add files via upload

---
+++ accept 5 no. and print.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+    int arr[5];
+    int i;
+    printf("\n\nRead and Print elements of an array:\n");
+    printf("-----------------------------------------\n");
+
+    printf("Input 5 elements in the array :\n");
+    for(i=0; i<5; i++)
+    {
+        printf("element - %d : ",i);
+        scanf("%d", &arr[i]);
+    }
+    printf("\nElements in array are: ");
+    for(i=0; i<5; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+    printf("\n");
+}
