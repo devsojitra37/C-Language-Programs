14df56ac883c776757429413b7c10dac7ade8438
devsojitra37 <devanshsojitra37@gmail.com>
2024-07-08 22:04:33 +0530

    Add files via upload

---
+++ find absolute value of given no func.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int m = abs(200);
+    int n = abs(-400);
+
+    printf("Absolute value of m = %d\n", m);
+    printf("Absolute value of n = %d \n",n);
+
+    return 0;
+}
